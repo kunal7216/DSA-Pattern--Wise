94d478b4daa1699ac46d787790b4f7c097175b84
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-05-31 02:21:38 +0530

    Update LongRepCharReplacement.cpp

--- Sliding-Window/LongRepCharReplacement.cpp
+++ Sliding-Window/LongRepCharReplacement.cpp
@@ -44,3 +44,121 @@ public:
         return maxlen;
     }
 };
+
+****************************************************************Brute solution***************************************************************************
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int ans = 0;
+
+        // Try every starting index
+        for (int i = 0; i < n; i++) {
+
+            // Try every ending index
+            for (int j = i; j < n; j++) {
+
+                vector<int> freq(26, 0);
+
+                // Count frequency of characters in substring s[i...j]
+                for (int x = i; x <= j; x++) {
+                    freq[s[x] - 'A']++;
+                }
+
+                // Find the highest frequency character
+                int maxFreq = 0;
+                for (int c = 0; c < 26; c++) {
+                    maxFreq = max(maxFreq, freq[c]);
+                }
+
+                int windowLength = j - i + 1;
+
+                // Characters other than the most frequent one need replacement
+                int replacementsNeeded = windowLength - maxFreq;
+
+                if (replacementsNeeded <= k) {
+                    ans = max(ans, windowLength);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+
+****************************************************************Better Solution***************************************************************************
+    #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        int n = s.size();
+        int ans = 0;
+
+        // Fix starting index
+        for (int i = 0; i < n; i++) {
+
+            vector<int> freq(26, 0);
+            int maxFreq = 0;
+
+            // Expand ending index
+            for (int j = i; j < n; j++) {
+
+                // Add current character to the current window
+                freq[s[j] - 'A']++;
+
+                // Update maximum frequency in this window
+                maxFreq = max(maxFreq, freq[s[j] - 'A']);
+
+                int windowLength = j - i + 1;
+                int replacementsNeeded = windowLength - maxFreq;
+
+                if (replacementsNeeded <= k) {
+                    ans = max(ans, windowLength);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
+************************************************************************Optimal solution***********************************************************************
+    #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int characterReplacement(string s, int k) {
+        vector<int> freq(26, 0);
+
+        int left = 0;
+        int maxFreq = 0;
+        int ans = 0;
+
+        for (int right = 0; right < s.size(); right++) {
+
+            // Add current character into the window
+            freq[s[right] - 'A']++;
+
+            // Maximum frequency of any character in the current window
+            maxFreq = max(maxFreq, freq[s[right] - 'A']);
+
+            int windowLength = right - left + 1;
+
+            // If more than k replacements are needed, shrink the window
+            if (windowLength - maxFreq > k) {
+                freq[s[left] - 'A']--;
+                left++;
+            }
+
+            // Update longest valid window size
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};

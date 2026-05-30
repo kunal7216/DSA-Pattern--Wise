04693a9d4730264150c3af38b4b815252e014e86
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-05-30 23:54:49 +0530

    Update LongSubStrWithoutRepChar.cpp
    Added another approach

--- Sliding-Window/LongSubStrWithoutRepChar.cpp
+++ Sliding-Window/LongSubStrWithoutRepChar.cpp
@@ -41,3 +41,31 @@ public:
         return maxLength;
     }
 };
+
+
+
+// Another method 
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        unordered_set<char> window;
+
+        int left = 0;
+        int maxLength = 0;
+
+        for (int right = 0; right < s.length(); right++) {
+            while (window.count(s[right])) {
+                window.erase(s[left]);
+                left++;
+            }
+
+            window.insert(s[right]);
+            maxLength = max(maxLength, right - left + 1);
+        }
+
+        return maxLength;
+    }
+};

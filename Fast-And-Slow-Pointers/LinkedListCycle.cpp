a4fed8d05575486620667554e941f7784565efe4
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-02 14:02:47 +0530

    Implement cycle detection in linked list with multiple solutions
    Added multiple solutions for detecting cycles in a linked list, including brute force, better, and optimal approaches.

--- Fast-And-Slow-Pointers/LinkedListCycle.cpp
+++ Fast-And-Slow-Pointers/LinkedListCycle.cpp
@@ -1,3 +1,87 @@
+
+****************************************************************************Brute Solution**************************************************************************
+    /**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        ListNode* curr = head;
+
+        // Pick each node one by one
+        while (curr != NULL) {
+            ListNode* temp = curr->next;
+
+            // Check whether curr appears again later
+            while (temp != NULL) {
+                if (temp == curr) {
+                    return true;
+                }
+
+                temp = temp->next;
+            }
+
+            curr = curr->next;
+        }
+
+        return false;
+    }
+};
+****************************************************************************Better Solution**************************************************************************
+    #include <unordered_set>
+using namespace std;
+
+/**
+ * Definition for singly-linked list.
+ */
+class ListNode {
+public:
+    int val;
+    ListNode *next;
+
+    ListNode(int x) {
+        val = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        unordered_set<ListNode*> visited;
+
+        ListNode* curr = head;
+
+        while (curr != NULL) {
+            // If this node was already visited, cycle exists
+            if (visited.find(curr) != visited.end()) {
+                return true;
+            }
+
+            // Mark current node as visited
+            visited.insert(curr);
+
+            // Move to next node
+            curr = curr->next;
+        }
+
+        // Reached NULL, so no cycle
+        return false;
+    }
+};
+******************************************************************************Optimal Solution************************************************************************
+
 class Solution
 {
 public:

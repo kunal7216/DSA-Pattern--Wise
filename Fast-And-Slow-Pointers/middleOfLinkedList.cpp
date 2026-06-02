c200680d1745450df7a6a47e445c7be0c7d1a26f
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-02 13:11:07 +0530

    Add multiple solutions for finding middle of linked list
    Implemented three solutions to find the middle node of a linked list: brute force using a vector, a better solution counting nodes, and an optimal solution.

--- Fast-And-Slow-Pointers/middleOfLinkedList.cpp
+++ Fast-And-Slow-Pointers/middleOfLinkedList.cpp
@@ -1,3 +1,58 @@
+***************************************************************************Brute Solution***************************************************************************
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        // Store all linked list nodes inside a vector
+        vector<ListNode*> nodes;
+
+        ListNode* temp = head;
+
+        // Traverse the linked list and push every node into vector
+        while (temp != nullptr) {
+            nodes.push_back(temp);
+            temp = temp->next;
+        }
+
+        // Middle index is size / 2
+        // Odd length: exact middle
+        // Even length: second middle
+        int middleIndex = nodes.size() / 2;
+
+        return nodes[middleIndex];
+    }
+};
+**********************************************************************Better Solution********************************************************************************
+
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        int length = 0;
+
+        ListNode* temp = head;
+
+        // Step 1: Count the total number of nodes
+        while (temp != nullptr) {
+            length++;
+            temp = temp->next;
+        }
+
+        // Step 2: Calculate middle index
+        // For even length, this gives second middle
+        int middleIndex = length / 2;
+
+        // Step 3: Move from head to middle index
+        temp = head;
+
+        for (int i = 0; i < middleIndex; i++) {
+            temp = temp->next;
+        }
+
+        return temp;
+    }
+};
+    
+********************************************************************optimal Solution**********************************************************************************
 class Solution
 {
 public:

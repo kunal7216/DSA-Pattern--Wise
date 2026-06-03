ca7b95953e47d59ed32ae12d5051a9f2ee1a0d90
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-03 14:34:07 +0530

    Implement multiple solutions for subarray sum problem
    Added brute force, better, and optimal solutions for counting subarrays that sum to k.

--- prefix-sum-And-Diff-Array/SubarraySumEqualsK.cpp
+++ prefix-sum-And-Diff-Array/SubarraySumEqualsK.cpp
@@ -42,3 +42,98 @@ public:
         return cmt;
     }
 };
+
+****************************************************************Brute Solution**************************************************************************************
+    #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+
+        // Try every starting index
+        for (int i = 0; i < n; i++) {
+            // Try every ending index
+            for (int j = i; j < n; j++) {
+                int sum = 0;
+
+                // Calculate sum of subarray nums[i...j]
+                for (int x = i; x <= j; x++) {
+                    sum += nums[x];
+                }
+
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+****************************************************************Better Solution**************************************************************************************
+    #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n = nums.size();
+        int count = 0;
+
+        // Fix starting index
+        for (int i = 0; i < n; i++) {
+            int sum = 0;
+
+            // Extend ending index
+            for (int j = i; j < n; j++) {
+                sum += nums[j];
+
+                // nums[i...j] has sum k
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+
+        return count;
+    }
+};
+****************************************************************Optimal Solution**************************************************************************************
+    #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> freq;
+
+        // Empty prefix sum exists once
+        // This helps count subarrays starting from index 0
+        freq[0] = 1;
+
+        int prefixSum = 0;
+        int count = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            prefixSum += nums[i];
+
+            // We need a previous prefix sum:
+            // previousPrefixSum = prefixSum - k
+            int need = prefixSum - k;
+
+            // If this prefix sum appeared before,
+            // all those occurrences create valid subarrays ending at i
+            if (freq.find(need) != freq.end()) {
+                count += freq[need];
+            }
+
+            // Store current prefix sum for future subarrays
+            freq[prefixSum]++;
+        }
+
+        return count;
+    }
+};

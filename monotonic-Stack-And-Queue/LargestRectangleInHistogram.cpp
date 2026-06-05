293e0861cfe3ec97d64def6e8cdc331bf2240913
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-05 11:57:00 +0530

    Update LargestRectangleInHistogram.cpp

--- monotonic-Stack-And-Queue/LargestRectangleInHistogram.cpp
+++ monotonic-Stack-And-Queue/LargestRectangleInHistogram.cpp
@@ -67,3 +67,133 @@ public:
         return maxArea;
     }
 };
+
+******************************************************************Brute solution*********************************************************************************
+    #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int maxArea = 0;
+
+        // Try every starting index
+        for (int i = 0; i < n; i++) {
+
+            // Try every ending index
+            for (int j = i; j < n; j++) {
+
+                // Find minimum height between i and j
+                int minHeight = INT_MAX;
+
+                for (int k = i; k <= j; k++) {
+                    minHeight = min(minHeight, heights[k]);
+                }
+
+                // Width of rectangle from i to j
+                int width = j - i + 1;
+
+                // Area using minimum height
+                int area = minHeight * width;
+
+                // Update answer
+                maxArea = max(maxArea, area);
+            }
+        }
+
+        return maxArea;
+    }
+};
+******************************************************************Better solution*********************************************************************************
+    #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+        int maxArea = 0;
+
+        // Fix starting index
+        for (int i = 0; i < n; i++) {
+
+            // Minimum height from i to j
+            int minHeight = INT_MAX;
+
+            // Extend ending index
+            for (int j = i; j < n; j++) {
+
+                // Update minimum height in current range
+                minHeight = min(minHeight, heights[j]);
+
+                // Width from i to j
+                int width = j - i + 1;
+
+                // Area of rectangle
+                int area = minHeight * width;
+
+                // Store maximum answer
+                maxArea = max(maxArea, area);
+            }
+        }
+
+        return maxArea;
+    }
+};
+******************************************************************Optimal Solution*********************************************************************************
+    #include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int largestRectangleArea(vector<int>& heights) {
+        int n = heights.size();
+
+        // Stack stores indices of bars
+        stack<int> st;
+
+        int maxArea = 0;
+
+        // We go till n inclusive to process remaining bars
+        for (int i = 0; i <= n; i++) {
+
+            // For i == n, we use height 0 to empty the stack
+            int currentHeight = (i == n) ? 0 : heights[i];
+
+            // If current bar is smaller, then stack top bar's rectangle ends here
+            while (!st.empty() && currentHeight < heights[st.top()]) {
+
+                // Height of rectangle
+                int height = heights[st.top()];
+                st.pop();
+
+                // Right boundary is i
+                int rightSmaller = i;
+
+                // Left boundary is stack top after popping
+                int leftSmaller;
+
+                if (st.empty()) {
+                    leftSmaller = -1;
+                } else {
+                    leftSmaller = st.top();
+                }
+
+                // Width where current height can extend
+                int width = rightSmaller - leftSmaller - 1;
+
+                // Calculate area
+                int area = height * width;
+
+                // Update answer
+                maxArea = max(maxArea, area);
+            }
+
+            // Push current index
+            st.push(i);
+        }
+
+        return maxArea;
+    }
+};

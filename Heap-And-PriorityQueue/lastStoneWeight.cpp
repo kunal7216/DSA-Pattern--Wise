cfc0bcd73a022c0bc61742ac685bcaa83eeb47b5
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-06 16:29:45 +0530

    Optimize lastStoneWeight with priority queue
    Refactored lastStoneWeight function to use a priority queue for optimal performance. Added brute force and better solutions for comparison.

--- Heap-And-PriorityQueue/lastStoneWeight.cpp
+++ Heap-And-PriorityQueue/lastStoneWeight.cpp
@@ -41,3 +41,97 @@ public:
         return pq.empty() ? 0 : pq.top();
     }
 };
+
+**************************************************************Brute Solution******************************************************************************
+    class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while (stones.size() > 1) {
+            int maxIndex1 = 0;
+
+            for (int i = 1; i < stones.size(); i++) {
+                if (stones[i] > stones[maxIndex1]) {
+                    maxIndex1 = i;
+                }
+            }
+
+            int first = stones[maxIndex1];
+            stones.erase(stones.begin() + maxIndex1);
+
+            int maxIndex2 = 0;
+
+            for (int i = 1; i < stones.size(); i++) {
+                if (stones[i] > stones[maxIndex2]) {
+                    maxIndex2 = i;
+                }
+            }
+
+            int second = stones[maxIndex2];
+            stones.erase(stones.begin() + maxIndex2);
+
+            if (first != second) {
+                stones.push_back(first - second);
+            }
+        }
+
+        if (stones.empty()) {
+            return 0;
+        }
+
+        return stones[0];
+    }
+};
+**************************************************************Better Solution******************************************************************************
+    class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        while (stones.size() > 1) {
+            sort(stones.begin(), stones.end());
+
+            int first = stones.back();
+            stones.pop_back();
+
+            int second = stones.back();
+            stones.pop_back();
+
+            if (first != second) {
+                stones.push_back(first - second);
+            }
+        }
+
+        if (stones.empty()) {
+            return 0;
+        }
+
+        return stones[0];
+    }
+};
+**************************************************************Optimal Solution******************************************************************************
+    class Solution {
+public:
+    int lastStoneWeight(vector<int>& stones) {
+        priority_queue<int> maxHeap;
+
+        for (int stone : stones) {
+            maxHeap.push(stone);
+        }
+
+        while (maxHeap.size() > 1) {
+            int first = maxHeap.top();
+            maxHeap.pop();
+
+            int second = maxHeap.top();
+            maxHeap.pop();
+
+            if (first != second) {
+                maxHeap.push(first - second);
+            }
+        }
+
+        if (maxHeap.empty()) {
+            return 0;
+        }
+
+        return maxHeap.top();
+    }
+};

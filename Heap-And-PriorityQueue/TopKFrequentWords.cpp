500c40bc96564347f979499300c4b5938dbaedd7
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-07 23:51:42 +0530

    Implement multiple solutions for top K frequent words
    Added brute force and optimal solutions for finding top K frequent words.

--- Heap-And-PriorityQueue/TopKFrequentWords.cpp
+++ Heap-And-PriorityQueue/TopKFrequentWords.cpp
@@ -68,3 +68,128 @@ public:
         return vec;
     }
 };
+
+******************************************************************Brute Solution*************************************************************************
+
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<pair<string, int>> freq;
+
+        for (int i = 0; i < words.size(); i++) {
+            bool alreadyAdded = false;
+
+            for (int j = 0; j < freq.size(); j++) {
+                if (freq[j].first == words[i]) {
+                    alreadyAdded = true;
+                    break;
+                }
+            }
+
+            if (alreadyAdded) continue;
+
+            int count = 0;
+
+            for (int j = 0; j < words.size(); j++) {
+                if (words[j] == words[i]) {
+                    count++;
+                }
+            }
+
+            freq.push_back({words[i], count});
+        }
+
+        sort(freq.begin(), freq.end(), [](auto &a, auto &b) {
+            if (a.second == b.second) {
+                return a.first < b.first;
+            }
+            return a.second > b.second;
+        });
+
+        vector<string> ans;
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(freq[i].first);
+        }
+
+        return ans;
+    }
+};
+******************************************************************Better Solution*************************************************************************
+
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> freq;
+
+        for (string word : words) {
+            freq[word]++;
+        }
+
+        vector<string> uniqueWords;
+
+        for (auto it : freq) {
+            uniqueWords.push_back(it.first);
+        }
+
+        sort(uniqueWords.begin(), uniqueWords.end(), [&](string &a, string &b) {
+            if (freq[a] == freq[b]) {
+                return a < b;
+            }
+            return freq[a] > freq[b];
+        });
+
+        vector<string> ans;
+
+        for (int i = 0; i < k; i++) {
+            ans.push_back(uniqueWords[i]);
+        }
+
+        return ans;
+    }
+};
+******************************************************************optimal Solution*************************************************************************
+
+
+class Solution {
+public:
+    struct Compare {
+        bool operator()(pair<string, int>& a, pair<string, int>& b) {
+            if (a.second == b.second) {
+                return a.first < b.first;
+            }
+            return a.second > b.second;
+        }
+    };
+
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> freq;
+
+        for (string word : words) {
+            freq[word]++;
+        }
+
+        priority_queue<pair<string, int>, vector<pair<string, int>>, Compare> pq;
+
+        for (auto it : freq) {
+            pq.push({it.first, it.second});
+
+            if (pq.size() > k) {
+                pq.pop();
+            }
+        }
+
+        vector<string> ans;
+
+        while (!pq.empty()) {
+            ans.push_back(pq.top().first);
+            pq.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+
+        return ans;
+    }
+};

799766a5f6bd3d8d3da175f2a92d4668a5439803
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-08 11:35:30 +0530

    Implement multiple solutions for reorganizing string
    Added brute force, better, and optimal solutions for reorganizing a string such that no two adjacent characters are the same.

--- Heap-And-PriorityQueue/reOrganizeString.cpp
+++ Heap-And-PriorityQueue/reOrganizeString.cpp
@@ -60,3 +60,133 @@ public:
         return res;
     }
 };
+
+*****************************************************************Brute Solution********************************************************************************
+    class Solution {
+public:
+    bool isValid(string s) {
+        for (int i = 1; i < s.size(); i++) {
+            if (s[i] == s[i - 1]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    string reorganizeString(string s) {
+        sort(s.begin(), s.end());
+
+        do {
+            if (isValid(s)) {
+                return s;
+            }
+        } while (next_permutation(s.begin(), s.end()));
+
+        return "";
+    }
+};
+*****************************************************************Better Solution********************************************************************************
+
+    class Solution {
+public:
+    string reorganizeString(string s) {
+        int n = s.size();
+
+        vector<int> freq(26, 0);
+
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        int maxFreq = 0;
+        for (int f : freq) {
+            maxFreq = max(maxFreq, f);
+        }
+
+        if (maxFreq > (n + 1) / 2) {
+            return "";
+        }
+
+        string ans = "";
+
+        for (int i = 0; i < n; i++) {
+            int bestChar = -1;
+            int bestFreq = 0;
+
+            for (int c = 0; c < 26; c++) {
+                if (freq[c] > bestFreq) {
+                    char currentChar = 'a' + c;
+
+                    if (ans.empty() || ans.back() != currentChar) {
+                        bestFreq = freq[c];
+                        bestChar = c;
+                    }
+                }
+            }
+
+            ans += char('a' + bestChar);
+            freq[bestChar]--;
+        }
+
+        return ans;
+    }
+};
+*****************************************************************Optimal Solution********************************************************************************
+
+    class Solution {
+public:
+    string reorganizeString(string s) {
+        int n = s.size();
+
+        vector<int> freq(26, 0);
+
+        for (char ch : s) {
+            freq[ch - 'a']++;
+        }
+
+        for (int f : freq) {
+            if (f > (n + 1) / 2) {
+                return "";
+            }
+        }
+
+        priority_queue<pair<int, char>> maxHeap;
+
+        for (int i = 0; i < 26; i++) {
+            if (freq[i] > 0) {
+                maxHeap.push({freq[i], char('a' + i)});
+            }
+        }
+
+        string ans = "";
+
+        while (maxHeap.size() >= 2) {
+            auto first = maxHeap.top();
+            maxHeap.pop();
+
+            auto second = maxHeap.top();
+            maxHeap.pop();
+
+            ans += first.second;
+            ans += second.second;
+
+            first.first--;
+            second.first--;
+
+            if (first.first > 0) {
+                maxHeap.push(first);
+            }
+
+            if (second.first > 0) {
+                maxHeap.push(second);
+            }
+        }
+
+        if (!maxHeap.empty()) {
+            ans += maxHeap.top().second;
+        }
+
+        return ans;
+    }
+};
+

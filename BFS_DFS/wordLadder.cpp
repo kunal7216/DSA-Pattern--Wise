0f988ebcb1209c24edc4be8fdfa79b0cf9ec375b
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-09 13:53:11 +0530

    Implement multiple solutions for word ladder problem
    Added multiple solutions for the word ladder problem, including brute force, better, and optimal approaches.

--- BFS_DFS/wordLadder.cpp
+++ BFS_DFS/wordLadder.cpp
@@ -151,4 +151,189 @@ class Solution {
             return 0;
         }
     };
-    
+
+
+*************************************************************Brute Solution*************************************************************************************
+    class Solution {
+public:
+    bool oneCharDiff(string &a, string &b) {
+        int diff = 0;
+
+        for (int i = 0; i < a.size(); i++) {
+            if (a[i] != b[i]) {
+                diff++;
+            }
+
+            if (diff > 1) {
+                return false;
+            }
+        }
+
+        return diff == 1;
+    }
+
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        int n = wordList.size();
+
+        bool found = false;
+        for (string &word : wordList) {
+            if (word == endWord) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            return 0;
+        }
+
+        vector<string> words = wordList;
+        words.push_back(beginWord);
+
+        int total = words.size();
+
+        vector<vector<int>> graph(total);
+
+        for (int i = 0; i < total; i++) {
+            for (int j = i + 1; j < total; j++) {
+                if (oneCharDiff(words[i], words[j])) {
+                    graph[i].push_back(j);
+                    graph[j].push_back(i);
+                }
+            }
+        }
+
+        int start = total - 1;
+        int target = -1;
+
+        for (int i = 0; i < total; i++) {
+            if (words[i] == endWord) {
+                target = i;
+                break;
+            }
+        }
+
+        queue<pair<int, int>> q;
+        vector<int> visited(total, 0);
+
+        q.push({start, 1});
+        visited[start] = 1;
+
+        while (!q.empty()) {
+            auto [node, level] = q.front();
+            q.pop();
+
+            if (node == target) {
+                return level;
+            }
+
+            for (int nei : graph[node]) {
+                if (!visited[nei]) {
+                    visited[nei] = 1;
+                    q.push({nei, level + 1});
+                }
+            }
+        }
+
+        return 0;
+    }
+};
+*************************************************************Better Solution*************************************************************************************
+    class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+
+        if (st.find(endWord) == st.end()) {
+            return 0;
+        }
+
+        queue<pair<string, int>> q;
+
+        q.push({beginWord, 1});
+
+        if (st.find(beginWord) != st.end()) {
+            st.erase(beginWord);
+        }
+
+        while (!q.empty()) {
+            string word = q.front().first;
+            int level = q.front().second;
+            q.pop();
+
+            if (word == endWord) {
+                return level;
+            }
+
+            for (int i = 0; i < word.size(); i++) {
+                char originalChar = word[i];
+
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    word[i] = ch;
+
+                    if (st.find(word) != st.end()) {
+                        q.push({word, level + 1});
+                        st.erase(word);
+                    }
+                }
+
+                word[i] = originalChar;
+            }
+        }
+
+        return 0;
+    }
+};
+*************************************************************optimal Solution*************************************************************************************
+    class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+        unordered_set<string> wordSet(wordList.begin(), wordList.end());
+
+        if (wordSet.find(endWord) == wordSet.end()) {
+            return 0;
+        }
+
+        unordered_set<string> beginSet;
+        unordered_set<string> endSet;
+
+        beginSet.insert(beginWord);
+        endSet.insert(endWord);
+
+        int level = 1;
+
+        while (!beginSet.empty() && !endSet.empty()) {
+            if (beginSet.size() > endSet.size()) {
+                swap(beginSet, endSet);
+            }
+
+            unordered_set<string> nextSet;
+
+            for (string word : beginSet) {
+                for (int i = 0; i < word.size(); i++) {
+                    char originalChar = word[i];
+
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        word[i] = ch;
+
+                        if (endSet.find(word) != endSet.end()) {
+                            return level + 1;
+                        }
+
+                        if (wordSet.find(word) != wordSet.end()) {
+                            nextSet.insert(word);
+                            wordSet.erase(word);
+                        }
+                    }
+
+                    word[i] = originalChar;
+                }
+            }
+
+            beginSet = nextSet;
+            level++;
+        }
+
+        return 0;
+    }
+};

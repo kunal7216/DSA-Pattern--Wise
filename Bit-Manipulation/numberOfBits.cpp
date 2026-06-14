e703bbb62e236492e50275793ed52b1018bff505
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-14 22:39:30 +0530

    Refactor hammingWeight function with multiple solutions
    Refactored the hammingWeight function to improve efficiency by implementing multiple solutions, including brute force, better, and optimal methods for counting set bits.

--- Bit-Manipulation/numberOfBits.cpp
+++ Bit-Manipulation/numberOfBits.cpp
@@ -1,29 +1,70 @@
-// number of 1 bits
+// number of 1 bits 191
 // leetcode: https://leetcode.com/problems/number-of-1-bits/
 
-class Solution
-{
+*******************************************************Brute Solution****************************************************************************************
+    class Solution {
 public:
-    int hammingWeight(uint32_t n)
-    {
+    int hammingWeight(uint32_t n) {
+        string binary = "";
 
-        // Variable to count the number of set bits (1s)
-        int res = 0;
+        // Convert number to binary string
+        while (n > 0) {
+            int bit = n % 2;
 
-        // Loop through all 32 bits of the integer
-        for (int i = 0; i < 32; i++)
-        {
+            if (bit == 1) {
+                binary += '1';
+            } else {
+                binary += '0';
+            }
 
-            // Right shift n by i positions
-            // AND with 1 to check if the ith bit is set
-            if ((n >> i) & 1)
-            {
-                // If the bit is 1, increment the count
-                res += 1;
+            n = n / 2;
+        }
+
+        int count = 0;
+
+        // Count number of '1' characters
+        for (char ch : binary) {
+            if (ch == '1') {
+                count++;
             }
         }
 
-        // Return total count of set bits
-        return res;
+        return count;
+    }
+};
+*******************************************************Better Solution****************************************************************************************
+    class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+
+        while (n > 0) {
+            // If last bit is 1, increase count
+            if (n & 1) {
+                count++;
+            }
+
+            // Remove the last bit
+            n = n >> 1;
+        }
+
+        return count;
+    }
+};
+*******************************************************Optimal Solution****************************************************************************************
+    class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int count = 0;
+
+        while (n != 0) {
+            // Remove the rightmost set bit
+            n = n & (n - 1);
+
+            // One set bit was removed
+            count++;
+        }
+
+        return count;
     }
 };

6fc15bc1da2e615d808df6b60b5934cc5170e0cf
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-15 22:34:40 +0530

    Implement multiple solutions for power of two check
    Added brute, better, and optimal solutions for checking if a number is a power of two.

--- Bit-Manipulation/powerOfTwo.cpp
+++ Bit-Manipulation/powerOfTwo.cpp
@@ -1,6 +1,54 @@
 // power of two
 // leetcode: https://leetcode.com/problems/power-of-two/
 
+
+**********************************************************Brute Solution**********************************************************************************
+    class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        // Negative numbers and 0
+        // cannot be powers of 2
+        if (n <= 0) {
+            return false;
+        }
+
+        // Keep dividing by 2
+        while (n % 2 == 0) {
+            n = n / 2;
+        }
+
+        // If finally becomes 1
+        // then it is power of 2
+        return n == 1;
+    }
+};
+**********************************************************Better Solution**********************************************************************************
+    class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+
+        if (n <= 0) {
+            return false;
+        }
+
+        int count = 0;
+
+        while (n > 0) {
+
+            // Check last bit
+            if (n & 1) {
+                count++;
+            }
+
+            n = n >> 1;
+        }
+
+        return count == 1;
+    }
+};
+**********************************************************Optimal Solution**********************************************************************************
+
 class Solution {
 public: 
     bool isPowerOfTwo(int n) {

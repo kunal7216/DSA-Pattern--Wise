0dacafb16614a4ccc6dbee94840cb94b935c5e40
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-06-17 11:26:05 +0530

    Implement multiple solutions for meeting rooms problem
    Refactored meeting room allocation logic with multiple solutions.

--- Interval-Merging/meetingRoomsII.cpp
+++ Interval-Merging/meetingRoomsII.cpp
@@ -1,28 +1,102 @@
 // meeting rooms problem link: https://leetcode.com/problems/meeting-rooms/
 //find mini rooms required to hold all meetings
 
+***************************************************Brute Solution***********************************************************************************
+    class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
 
+        sort(intervals.begin(), intervals.end());
 
-int solution(vector<vector<int>> &intervals)
-{
-    // Sort intervals based on start time
-    sort(intervals.begin(), intervals.end());
+        vector<int> rooms;
 
-    // Min-heap to track the end times of meetings
-    priority_queue<int, vector<int>, greater<int>> minHeap;
+        for (auto& meeting : intervals) {
 
-    for (const auto &interval : intervals)
-    {
-        // If the earliest ending meeting is done before the current meeting starts
-        if (!minHeap.empty() && minHeap.top() <= interval[0])
-        {
-            minHeap.pop(); // Reuse the room
+            int start = meeting[0];
+            int end = meeting[1];
+
+            bool placed = false;
+
+            for (int i = 0; i < rooms.size(); i++) {
+
+                if (rooms[i] <= start) {
+                    rooms[i] = end;
+                    placed = true;
+                    break;
+                }
+            }
+
+            if (!placed)
+                rooms.push_back(end);
         }
 
-        // Allocate a new room (or reuse) by adding the end time of the current meeting
-        minHeap.push(interval[1]);
+        return rooms.size();
     }
+};
+***************************************************Better Solution***********************************************************************************
+    class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
 
-    // The size of the heap is the number of rooms required
-    return minHeap.size();
-}
+        int n = intervals.size();
+
+        vector<int> starts(n);
+        vector<int> ends(n);
+
+        for (int i = 0; i < n; i++) {
+            starts[i] = intervals[i][0];
+            ends[i] = intervals[i][1];
+        }
+
+        sort(starts.begin(), starts.end());
+        sort(ends.begin(), ends.end());
+
+        int rooms = 0;
+        int endPtr = 0;
+
+        for (int startPtr = 0; startPtr < n; startPtr++) {
+
+            if (starts[startPtr] < ends[endPtr]) {
+                rooms++;
+            } else {
+                endPtr++;
+            }
+        }
+
+        return rooms;
+    }
+};
+***************************************************Optimal Solution***********************************************************************************
+
+class Solution {
+public:
+    int minMeetingRooms(vector<vector<int>>& intervals) {
+
+        if (intervals.empty())
+            return 0;
+
+        sort(intervals.begin(), intervals.end());
+
+        priority_queue<
+            int,
+            vector<int>,
+            greater<int>
+        > minHeap;
+
+        minHeap.push(intervals[0][1]);
+
+        for (int i = 1; i < intervals.size(); i++) {
+
+            int start = intervals[i][0];
+            int end = intervals[i][1];
+
+            if (minHeap.top() <= start) {
+                minHeap.pop();
+            }
+
+            minHeap.push(end);
+        }
+
+        return minHeap.size();
+    }
+};

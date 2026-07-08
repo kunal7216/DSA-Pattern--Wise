5f0fc04b5ff2e6eca45b1c0ca4c8d7bfa935afd9
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-07-08 14:58:53 +0530

    Refactor subsets solution with backtracking

--- Backtracking/subsetsI.cpp
+++ Backtracking/subsetsI.cpp
@@ -2,47 +2,96 @@
 // link: https://leetcode.com/problems/subsets/
 //leetcode 78
 
-class Solution
-{
+// Brute Solution 
+class Solution {
 public:
-    vector<vector<int>> subsets(vector<int> &nums)
-    {
 
-        vector<vector<int>> res; // Stores all subsets
-        vector<int> subset;      // Current subset being built
+    vector<vector<int>> ans;
 
-        // Start backtracking from index 0
-        createSubset(nums, 0, res, subset);
+    void solve(int index, vector<int>& nums, vector<int>& curr) {
 
-        return res;
-    }
-
-    void createSubset(vector<int> &nums, int index,
-                      vector<vector<int>> &res,
-                      vector<int> &subset)
-    {
-
-        // Base case:
-        // If we have processed all elements,
-        // store the current subset and return
-        if (index == nums.size())
-        {
-            res.push_back(subset);
+        if (index == nums.size()) {
+            ans.push_back(curr);
             return;
         }
 
-        // ---------- INCLUDE CURRENT ELEMENT ----------
-        // Choose the current element
-        subset.push_back(nums[index]);
+        // Take
+        curr.push_back(nums[index]);
+        solve(index + 1, nums, curr);
 
-        // Move to the next index
-        createSubset(nums, index + 1, res, subset);
+        // Backtrack
+        curr.pop_back();
 
-        // ---------- EXCLUDE CURRENT ELEMENT ----------
-        // Backtrack: remove last added element
-        subset.pop_back();
+        // Don't take
+        solve(index + 1, nums, curr);
+    }
 
-        // Skip the current element and move forward
-        createSubset(nums, index + 1, res, subset);
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<int> curr;
+
+        solve(0, nums, curr);
+
+        return ans;
     }
 };
+// Better Solution ---recursive include / exclude
+
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<vector<int>> ans;
+        int n = nums.size();
+
+        int total = 1 << n;
+
+        for (int mask = 0; mask < total; mask++) {
+
+            vector<int> subset;
+
+            for (int i = 0; i < n; i++) {
+
+                if (mask & (1 << i))
+                    subset.push_back(nums[i]);
+            }
+
+            ans.push_back(subset);
+        }
+
+        return ans;
+    }
+};
+// optimal solution --- backtracking using loop
+
+class Solution {
+public:
+
+    vector<vector<int>> ans;
+
+    void backtrack(int start, vector<int>& nums, vector<int>& curr) {
+
+        // Every current subset is valid
+        ans.push_back(curr);
+
+        for (int i = start; i < nums.size(); i++) {
+
+            curr.push_back(nums[i]);
+
+            backtrack(i + 1, nums, curr);
+
+            // Undo the choice
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+
+        vector<int> curr;
+
+        backtrack(0, nums, curr);
+
+        return ans;
+    }
+};
+

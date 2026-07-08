8cb7fb1df3ab55729b5aa462ae7964f17b99a8a4
Kunal kumar <112888767+kunal7216@users.noreply.github.com>
2026-07-08 17:27:46 +0530

    Refactor permutations II with optimal solution
    Refactor permutations II solution to improve efficiency and clarity. Introduce frequency maps for optimal solution and clean up existing backtracking methods.

--- Backtracking/permutationsII.cpp
+++ Backtracking/permutationsII.cpp
@@ -2,43 +2,137 @@
 // link: https://leetcode.com/problems/permutations-ii/
 // leetcode 47
 
-class Solution
-{
+// Brute Solution 
+class Solution {
 public:
-    vector<vector<int>> permuteUnique(vector<int> &nums)
-    {
-        vector<vector<int>> perms;
-        vector<int> solution;
-        vector<bool> used(nums.size(), false);
-        sort(nums.begin(), nums.end()); // sort to handle duplicates
-        backtrack(nums, used, solution, perms);
-        return perms;
-    }
+    vector<vector<int>> ans;
+    set<vector<int>> st;
 
-private:
-    void backtrack(vector<int> &nums, vector<bool> &used, vector<int> &solution, vector<vector<int>> &perms)
-    {
-        if (solution.size() == nums.size())
-        {
-            perms.push_back(solution);
+    void backtrack(vector<int>& nums, vector<int>& curr,
+                   vector<bool>& used) {
+
+        if (curr.size() == nums.size()) {
+            st.insert(curr);
             return;
         }
 
-        for (int i = 0; i < nums.size(); i++)
-        {
-            // Skip if already used
+        for (int i = 0; i < nums.size(); i++) {
+
             if (used[i])
                 continue;
 
-            // Skip duplicates: only use the first unused duplicate
-            if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1])
+            used[i] = true;
+            curr.push_back(nums[i]);
+
+            backtrack(nums, curr, used);
+
+            curr.pop_back();
+            used[i] = false;
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+        vector<int> curr;
+        vector<bool> used(nums.size(), false);
+
+        backtrack(nums, curr, used);
+
+        for (auto x : st)
+            ans.push_back(x);
+
+        return ans;
+    }
+};
+
+// Better Solution
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void solve(vector<int>& nums,
+               vector<int>& curr,
+               vector<bool>& used) {
+
+        if (curr.size() == nums.size()) {
+            ans.push_back(curr);
+            return;
+        }
+
+        for (int i = 0; i < nums.size(); i++) {
+
+            if (used[i])
                 continue;
 
-            solution.push_back(nums[i]);
+            if (i > 0 &&
+                nums[i] == nums[i - 1] &&
+                !used[i - 1])
+                continue;
+
             used[i] = true;
-            backtrack(nums, used, solution, perms);
+            curr.push_back(nums[i]);
+
+            solve(nums, curr, used);
+
+            curr.pop_back();
             used[i] = false;
-            solution.pop_back();
         }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+        sort(nums.begin(), nums.end());
+
+        vector<int> curr;
+        vector<bool> used(nums.size(), false);
+
+        solve(nums, curr, used);
+
+        return ans;
+    }
+};
+
+
+// Optimal Solution -- frequency maps
+class Solution {
+public:
+    vector<vector<int>> ans;
+
+    void solve(unordered_map<int, int>& freq,
+               vector<int>& curr,
+               int n) {
+
+        if (curr.size() == n) {
+            ans.push_back(curr);
+            return;
+        }
+
+        for (auto &it : freq) {
+
+            if (it.second == 0)
+                continue;
+
+            curr.push_back(it.first);
+            it.second--;
+
+            solve(freq, curr, n);
+
+            it.second++;
+            curr.pop_back();
+        }
+    }
+
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+
+        unordered_map<int, int> freq;
+
+        for (int x : nums)
+            freq[x]++;
+
+        vector<int> curr;
+
+        solve(freq, curr, nums.size());
+
+        return ans;
     }
 };
